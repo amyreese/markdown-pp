cbd2f1aa5a782d27f17428cf675a2541734f892b
John Reese <jreese@leetcode.net>
2010-05-19 14:43:09 -0400

    Updated sample client

--- mdpp.py
+++ mdpp.py
@@ -4,13 +4,5 @@ import MarkdownPP
 mdpp = open("readme.mdpp","r")
 md = open("readme","w")
 
-classdef = MarkdownPP.modules["include"]
-module = classdef()
-
-pp = MarkdownPP.Processor()
-#pp.register(module)
-
-pp.input(mdpp)
-pp.process()
-pp.output(md)
+MarkdownPP.MarkdownPP(input=mdpp, output=md, modules=["tableofcontents"])
 
