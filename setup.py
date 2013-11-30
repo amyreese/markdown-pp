36401fb4c23b2671b2e6decf37b2c130d4fe504e
John Reese <john@noswap.com>
2013-11-30 11:19:12 -0800

    Update setup.py for setuptools + pypi

--- setup.py
+++ setup.py
@@ -2,13 +2,25 @@
 # -*- coding: utf-8 -*-
 
 
-from distutils.core import setup
+from setuptools import setup
 
 setup(name = "MarkdownPP",
+      description = "Markdown preprocessor",
       version = "1.0",
-      description = "Markdown",
       author = "John Reese",
       author_email = "john@noswap.com",
-      url = "https://github.com/jreese/markdown-pp.git",
-      data_files = [('/usr/local/bin', ['markdown-pp.py'])], 
-      packages = ['MarkdownPP', 'MarkdownPP/Modules'],)
+      url = "https://github.com/jreese/markdown-pp",
+      classifiers=['License :: OSI Approved :: MIT License',
+                   'Operating System :: OS Independent',
+                   'Programming Language :: Python',
+                   'Programming Language :: Python :: 2.7',
+                   'Programming Language :: Python :: 3',
+                   'Programming Language :: Python :: 3.2',
+                   'Programming Language :: Python :: 3.3',
+                   'Topic :: Utilities',
+                   'Development Status :: 4 - Beta',
+                   ],
+      license='MIT License',
+      scripts = ['markdown-pp.py'],
+      packages = ['MarkdownPP', 'MarkdownPP/Modules'],
+      )
