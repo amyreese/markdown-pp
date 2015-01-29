63c530a4b39c1d8b90e4384c8865d82750bbd92f
John Reese <john@noswap.com>
2015-01-28 17:49:47 -0800

    Version bump

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ if path.isfile('README.md'):
 
 setup(name="MarkdownPP",
       description="Markdown preprocessor",
-      version="1.0",
+      version="1.1",
       author="John Reese",
       author_email="john@noswap.com",
       url="https://github.com/jreese/markdown-pp",
