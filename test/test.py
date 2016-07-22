cf02fdd5b8d8f0eae0a0d7a6479ce974672ca094
Leonardo <Leonardo Torres>
2016-07-22 13:29:25 -0500

    add __future__ imports for unicode literals.

--- test/test.py
+++ test/test.py
@@ -6,6 +6,10 @@ Unit testing for MarkdownPP.
 
 """
 
+from __future__ import absolute_import
+from __future__ import print_function
+from __future__ import unicode_literals
+
 import unittest
 import os
 import re
