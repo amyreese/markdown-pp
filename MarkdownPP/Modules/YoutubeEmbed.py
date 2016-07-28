cc4846cc6a7311bb2d3ff976abf8a5e07aecedd5
Leonardo <Leonardo Torres>
2016-07-28 14:32:37 -0500

    safer except

--- MarkdownPP/Modules/YoutubeEmbed.py
+++ MarkdownPP/Modules/YoutubeEmbed.py
@@ -76,7 +76,7 @@ class YoutubeEmbed(Module):
             import urllib
             try:
                 urlretrieve = urllib.request.urlretrieve
-            except:
+            except ImportError:
                 urlretrieve = urllib.urlretrieve
 
             # create temporary files for image operations
