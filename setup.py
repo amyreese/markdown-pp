8be7d4b8a4f46159df49931357074a57524ab473
John Reese <john@noswap.com>
2017-01-19 14:06:43 -0800

    Reformat setup.py

--- setup.py
+++ setup.py
@@ -9,33 +9,35 @@ import shutil
 if path.isfile('README.md'):
     shutil.copyfile('README.md', 'README')
 
-setup(name="MarkdownPP",
-      description="Markdown preprocessor",
-      version="1.2",
-      author="John Reese",
-      author_email="john@noswap.com",
-      url="https://github.com/jreese/markdown-pp",
-      classifiers=['License :: OSI Approved :: MIT License',
-                   'Operating System :: OS Independent',
-                   'Programming Language :: Python',
-                   'Programming Language :: Python :: 2.6',
-                   'Programming Language :: Python :: 2.7',
-                   'Programming Language :: Python :: 3',
-                   'Programming Language :: Python :: 3.2',
-                   'Programming Language :: Python :: 3.3',
-                   'Programming Language :: Python :: 3.4',
-                   'Topic :: Utilities',
-                   'Development Status :: 4 - Beta',
-                   ],
-      license='MIT License',
-      # scripts=['bin/markdown-pp'],
-      packages=['MarkdownPP', 'MarkdownPP/Modules'],
-      entry_points={
-            'console_scripts': [
-                  'markdown-pp = bin.markdown_pp:main'
-            ],
-      },
-      install_requires=[
-            "Watchdog >= 0.8.3",
-      ],
-      )
+setup(
+    name='MarkdownPP',
+    description='Markdown preprocessor',
+    version='1.2',
+    author='John Reese',
+    author_email='john@noswap.com',
+    url='https://github.com/jreese/markdown-pp',
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Topic :: Utilities',
+        'Development Status :: 4 - Beta',
+    ],
+    license='MIT License',
+    # scripts=['bin/markdown-pp'],
+    packages=['MarkdownPP', 'MarkdownPP/Modules'],
+    entry_points={
+        'console_scripts': [
+            'markdown-pp = bin.markdown_pp:main'
+        ],
+    },
+    install_requires=[
+        'Watchdog >= 0.8.3',
+    ],
+)
